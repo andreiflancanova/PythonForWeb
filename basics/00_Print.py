b3df1d79affc69e323f275daad319900d8bef7f8
unknown <andreiflancanova@hotmail.com>
2022-08-30 01:43:01 -0300

    FEAT: 00 Print Command

---
+++ basics/00_Print.py
@@ -0,0 +1,30 @@
+# Criando um Hello World em Python
+print('Hello World!')
+
+# Python é uma linguagem interpretada.
+
+"""
+    Comentários de múltiplas linhas são criados com DocStrings
+"""
+
+"""
+    Ao final de um código, deixar uma linha em branco é boa prática
+"""
+
+#Comando Print
+
+print("Python", "Learning", "Test")
+
+#Alterando sep
+print("Python", "Learning", "Test", sep="_")
+
+#Alterando end
+print("Python", "Learning", "Test", end=" ")
+print("End vazio fica na mesma linha")
+
+#Criando número padrão CPF
+
+print("123","456","789",sep=".", end="-")
+
+#Criar comando de aspas dentro do print
+print("This is a 'test'")
